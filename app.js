fc9097afdb2e1ec5ff302a0ea92cc0c62b87e1f7
freshcodeAlice <freshjs.ofte@gmail.com>
2024-02-12 09:14:27 +0200

    init template structure

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const rootRouter = require('./routes/rootRouter');
+const app = express();
+
+const bodyParser = express.json();
+
+app.use(bodyParser);
+app.use('/api', rootRouter);
+
+module.exports = app;
