769472a9ad93ef5da05d1f47cba1a5c0a21f5a99
freshcodeAlice <freshjs.ofte@gmail.com>
2024-02-12 12:02:04 +0200

    update

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -4,7 +4,7 @@ module.exports.createOne = async (req, res, next) => {
     try {
         const {body} = req;
         const user = await User.create(body);
-        res.status(201).send(user);
+        res.status(201).send({data: user});
     } catch(error) {
         res.status(400).send(error);
     }
@@ -13,7 +13,7 @@ module.exports.createOne = async (req, res, next) => {
 module.exports.getAll = async (req, res, next) => {
     try {
         const users = await User.findAll();
-        res.status(200).send(users);
+        res.status(200).send({data: users});
     } catch (error) {
 
     }
@@ -24,13 +24,25 @@ module.exports.getOne = async (req, res, next) => {
         const {params: {userId}} = req;
         // дописати контроллер на отримання 1 юзера за його PrimaryKey
         const user = await User.findByPk(userId);
-        res.status(200).send(user);
+        res.status(200).send({data: user});
     } catch(error) {
 
     }
 }
 
-module.exports.updateOne = async (req, res, next) => {}
+module.exports.updateOne = async (req, res, next) => {
+    try {
+        const {body, params: {userId}} = req;
+        const updated = await User.update(body, {
+            where: {
+                id: Number(userId)
+            }
+        });
+        res.status(200).send({data: updated})
+    } catch(error) {
+
+    }
+}
 
 // module.exports.deleteOne = async (req, res, next) => {
 //     try { 
