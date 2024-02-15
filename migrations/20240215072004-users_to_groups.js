7a1b3ee03c2da8295f94790a5598881f6fa36142
freshcodeAlice <freshjs.ofte@gmail.com>
2024-02-15 09:36:47 +0200

    migrations groups

---
+++ migrations/20240215072004-users_to_groups.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.createTable('users_to_groups', { 
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      groupId: {
+        type: Sequelize.INTEGER,
+        allowNull: false ,
+        field: 'group_id',
+        references: {
+          model: 'groups',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        field: 'update_at',
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+
+     });
+
+     await queryInterface.addConstraint('users_to_groups', {
+      fields: ['user_id', 'group_id'],
+      type: 'unique',
+      name: 'unique_pair_constraint'
+     })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('users_to_groups');
+  }
+};
